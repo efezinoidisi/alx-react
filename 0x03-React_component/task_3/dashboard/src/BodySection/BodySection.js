5065ad0b23763b9528db24117af20f8505e4e737
efezino <enduranceidisi13@gmailcom>
2023-08-11 01:19:21 +0100

    complete task 3

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class BodySection extends Component {
+  render() {
+    const { title, children } = this.props;
+    return (
+      <div className='bodySection'>
+        <h2>{title}</h2>
+        {children}
+      </div>
+    );
+  }
+}
+
+export default BodySection;
---
+++ 0x03-React_component/task_3/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className='App-header'>
+      <h1>School dashboard</h1>
+    </div>
+  );
+};
+
+export default Header;
