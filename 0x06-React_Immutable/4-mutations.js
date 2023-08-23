5e1b9ff9d6bbc4813736095b9fdfd049f977d14a
efezino <enduranceidisi13@gmailcom>
2023-08-23 18:23:17 +0100

    fix: use correct export

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,1 +1,13 @@
-import { List } from 'immutable';
+//import { Map } from 'immutable';
+const { Map } = require('immutable');
+
+const map = Map({
+  1: 'Liam',
+  2: 'Noah',
+  3: 'Elijah',
+  4: 'Oliver',
+  5: 'Jacob',
+  6: 'Lucas',
+});
+
+const map2 = {};
