b547c4e159d118ea5fe49a2c178d5d2a883aa64c
efezino <enduranceidisi13@gmailcom>
2023-08-23 19:28:33 +0100

    fix: spelling error

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const mapped = Map(object);
+  const mapped = fromJS(object);
   return mapped.getIn(array, undefined);
 }
