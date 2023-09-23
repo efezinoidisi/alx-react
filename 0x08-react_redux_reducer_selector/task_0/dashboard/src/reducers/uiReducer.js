b06f7a6ea2095df325f432e58cfa1fb53558008b
efezino <enduranceidisi13@gmailcom>
2023-09-23 17:04:30 +0100

    complete task one and two

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,35 @@
+import { 
+    LOGIN, 
+    LOGOUT,
+    DISPLAY_NOTIFICATION_DRAWER, 
+    HIDE_NOTIFICATION_DRAWER,
+    LOGIN_SUCCESS,LOGIN_FAILURE 
+} from "../actions/uiActionTypes";
+
+import { Map } from 'immutable';
+
+const initialState =Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: true,
+    user: Map(),
+});
+
+const uiReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case DISPLAY_NOTIFICATION_DRAWER:
+            return state.set('isNotificationDrawerVisible', true);
+        case HIDE_NOTIFICATION_DRAWER:
+            return state.set('isNotificationDrawerVisible', false);
+        case LOGIN_SUCCESS:
+            return state.set('isUserLoggedIn', true);
+        case LOGIN_FAILURE:
+            return state.set('isUserLoggedIn', false);
+        case LOGOUT:
+            return state.set('isUserLoggedIn', false);
+            
+        default:
+            return state;
+    }
+};
+
+export default uiReducer;
